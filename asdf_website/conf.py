6bedd4573b51520bf37a2c8b6f0f385569220e3d
alphasentaurii <rkein@stsci.edu>
2024-11-24 13:12:24 -0500

    RTD and project config additions

--- asdf_website/conf.py
+++ asdf_website/conf.py
@@ -30,7 +30,7 @@ release = configuration["version"]
 version = '0.0'
 
 # -- General configuration ---------------------------------------------------
-
+on_rtd = 'True'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
