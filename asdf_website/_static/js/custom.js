fb5c7c01bed883713dd006fa54558dd5d6329fef
alphasentaurii <rkein@stsci.edu>
2025-03-27 14:23:56 -0400

    wait for variable initialization

--- asdf_website/_static/js/custom.js
+++ asdf_website/_static/js/custom.js
@@ -8,8 +8,30 @@ function hideAds() {
     });
 }
 
-if (document.readyState === "loading"){
-    document.addEventListener('DOMContentLoaded', hideAds());
-} else {
-    hideAds();
+function waitForVariable(variableName, timeout=5000) {
+    return new Promise((resolve, reject) => {
+        const startTime = Date.now();
+        const intervalId = setInterval(() => {
+            if (typeof window[variableName] != 'undefined') {
+                clearInterval(intervalId);
+                resolve(window[variableName]);
+            } else if (Date.now() - startTime > timeout) {
+                clearInterval(intervalId);
+                reject(new Error(`Timeout waiting for ${variableName}`));
+            }
+        }, 100); // check every 100ms
+    });
 }
+
+waitForVariable('ethicalads')
+  .then((value) => {
+    console.log('Variable is defined:', value);
+    // proceed
+    hideAds();
+  })
+  .catch((error) => {
+    console.error(error);
+    // handle timeout or error
+  }, 2000);
+
+
